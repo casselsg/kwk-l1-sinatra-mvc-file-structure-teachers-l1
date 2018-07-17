377fda1254c698f8847ee2439ed806f0afddf03a
casselsg <casselsg19@bcdschool.org>
2018-07-17 17:58:05 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,21 @@
 class Dog
   # Replace with Dog clas
+  attr_accessor :name, :age, :breed
+  @@dog_array = []
   
+  def initialize(name,age,breed)
+    @name = name
+    @age = age
+    @breed = breed
+    @@dog_array << self
+  end
+  
+  def self.all
+    @@dog_array
+  end
   
 end
+
+
+pluto = Dog.new("Pluto",13,"hound")
+sadie = Dog.new("Sadie",13,"yellow lab")
