f39a13bca1536d677c635f3da730fe4efea9893c
casselsg <casselsg19@bcdschool.org>
2018-07-17 16:14:38 +0000

    automatically backed up by learn

---
+++ app/models/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  
+  attr_accessor :name, :breed, :age
+  
+  @@dog_count = []
+  
+  
+  def initialize(name,breed,age)
+    @name= name
+    @breed= breed
+    @age= age 
+    @@dog_all << self 
+
+ end
+
+def self.all 
+  @@dog_all
+end
+
+end
+
+
